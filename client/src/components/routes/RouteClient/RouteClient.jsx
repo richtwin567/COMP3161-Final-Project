f41c36fcbf4d89859f8b328932c327366fa31256
richtwin567 <anakai.richards@gmail.com>
2021-04-02 20:16:28 -0500

    redirrect to login on logout

--- client/src/components/routes/RouteClient/RouteClient.jsx
+++ client/src/components/routes/RouteClient/RouteClient.jsx
@@ -3,38 +3,41 @@ import { Switch, Route, Redirect } from "react-router-dom";
 
 import { MainLayout } from "components";
 import {
-  Auth,
-  ShoppingList,
-  Profile,
-  RecipeCatalogue,
-  MealPlan,
-  PlanGenerator,
+	Auth,
+	ShoppingList,
+	Profile,
+	RecipeCatalogue,
+	MealPlan,
+	PlanGenerator,
 } from "views";
 
 function RouteClient() {
-  return (
-    <Switch>
-      <Route exact path="/auth">
-        <Auth />
-      </Route>
-      <Route exact path="/app/overview">
-        <MainLayout component={Profile} />
-      </Route>
-      <Route exact path="/app/my-plan">
-        <MainLayout component={MealPlan} />
-      </Route>
-      <Route exact path="/app/plan-generator">
-        <MainLayout component={PlanGenerator} />
-      </Route>
-      <Route exact path="/app/recipes">
-        <MainLayout component={RecipeCatalogue} />
-      </Route>
-      <Route exact path="/app/shopping-list">
-        <MainLayout component={ShoppingList} />
-      </Route>
-      {/* //TODO not found*/}
-    </Switch>
-  );
+	return (
+		<Switch>
+			<Route exact path="/auth">
+				<Auth />
+			</Route>
+			<Route exact path="/app/overview">
+				<MainLayout component={Profile} />
+			</Route>
+			<Route exact path="/app/my-plan">
+				<MainLayout component={MealPlan} />
+			</Route>
+			<Route exact path="/app/plan-generator">
+				<MainLayout component={PlanGenerator} />
+			</Route>
+			<Route exact path="/app/recipes">
+				<MainLayout component={RecipeCatalogue} />
+			</Route>
+			<Route exact path="/app/shopping-list">
+				<MainLayout component={ShoppingList} />
+			</Route>
+			<Route exact path="/app/logout">
+				<Redirect to="/login" />
+			</Route>
+			{/* //TODO not found*/}
+		</Switch>
+	);
 }
 
 export default RouteClient;
